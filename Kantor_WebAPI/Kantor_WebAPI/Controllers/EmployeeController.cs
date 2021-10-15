0e21f1253054149a9d1e9a5b662721a12eef83b2
Mich <michsteam1234@gmail.com>
2021-10-15 11:32:32 +0700

    Sesi 9 Hacktiv8

---
+++ Kantor_WebAPI/Kantor_WebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Kantor_WebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Kantor_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            this._context = context;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItems()
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+            return _context.GetAllEmployee();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}", Name ="Get")]
+        public ActionResult<IEnumerable<EmployeeItem>> GetEmployeeItem(String id)
+        {
+            _context = HttpContext.RequestServices.GetService(typeof(EmployeeContext)) as EmployeeContext;
+            return _context.GetEmployee(id);
+        }
+
+        //// POST api/<controller>
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/<controller>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/<controller>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
